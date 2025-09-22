df647ca02e2426885901bce19326dd4bc7ae8c46
sanopi <36667265+sanopi@users.noreply.github.com>
2025-09-23 01:08:51 +0900

    bit演算のお試し

--- hello-world/helloworld.go
+++ hello-world/helloworld.go
@@ -7,5 +7,17 @@ const hello = "hello"
 func main() {
 	world := "world"
 	fmt.Println(hello + ", " + world + "!")
-	fmt.Printf("%c", 'a'+10) // 0-indexedで10番目のアルファベットが出力される。
+	fmt.Printf("%c\n", 'a'+10) // 0-indexedで10番目のアルファベットが出力される。
+
+	fmt.Println("1 & 2")
+	fmt.Println(1 & 2)
+	fmt.Println("1 &^ 2")
+	fmt.Println(1 &^ 2)
+	fmt.Println("2 &^ 1")
+	fmt.Println(2 &^ 1)
+	fmt.Println("1 &^ 1")
+	fmt.Println(1 &^ 1)
+	fmt.Println("0b111111 &^ 0b101010")
+	fmt.Println(0b111111 &^ 0b101010) // 0b010101 -> 16+4+1 = 21
+
 }
